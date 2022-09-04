711fb9dda018516780ba1258fc10343e122d4f7c
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 16:46:28 +0700

    Пользовательский ввод массива через строку

---
+++ EX007/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользовательский ввод массива через строку
+
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write($"{element} ");
+}
+
+Console.Write("Введите массив: ");
+// int[] array = Console.ReadLine()?.Split(' ').Select(int.Parse).ToArray();
+string[] strings = Console.ReadLine()?.Split(' '); //Разделение массива через пробелы
+int[] array = new int[strings.Length]; //Создание массива с длинной количества элементов в строке
+for(int i = 0; i < strings.Length; i++)
+    array[i] = int.Parse(strings[i]);  // Преобразование строки в число по элементно
+Console.Write("Ваш массив: ");
+PrintArray(array);
