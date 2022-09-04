1f0919a318ea8b7bd0d348e739e2fc7ad121b1d5
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 19:27:27 +0700

    Посчитать сумму цифр в числе

---
+++ EX010_DZ3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Посчитать сумму цифр в числе
+
+int Sum(int num, int sum)
+{
+    while(num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+return sum;
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int sum = Sum(num, 0);
+
+Console.WriteLine($"Сумма цифр в числе {num} = {sum}");
