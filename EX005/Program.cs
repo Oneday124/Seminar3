ab96e4ef55681f8f0928c48e24ca9cdf92229a9e
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 16:16:48 +0700

    Создать массив из 8 элементов и вывети их на экран. Решение без методов

---
+++ EX005/Program.cs
@@ -0,0 +1,14 @@
+﻿// Создать массив из 8 элементов и вывети их на экран. Решение без методов
+
+Console.WriteLine("Введите длинну массива: ");
+int lenght = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+
+int[] array = new int[lenght];
+int i = 0;
+while(i <= lenght - 1)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write($" {array[i]}");
+    i++;
+}
