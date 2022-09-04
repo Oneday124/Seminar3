588ccda67459e0d7cfe27e24e944cf9dfebd9510
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 19:47:34 +0700

    Написать программу вычисления произведения чисел от 1 до N

---
+++ EX011_DZ4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int Multiplication(int N, int mult)
+{
+    for(int i = 1; i <= N; i++)
+        mult = mult * i; 
+return mult;
+}
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int mult = Multiplication(N, 1);
+Console.WriteLine($"Произведение чисел от 1 до {N} = {mult}");
