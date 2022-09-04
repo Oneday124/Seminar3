7a953c84a5067737132d08b8a4871559e2424a49
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 13:57:42 +0700

    Показать таблицу квадратов чисел от 1 до N

--- EX001/Program.cs
+++ EX001/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Показать таблицу квадратов чисел от 1 до N
 
 Console.WriteLine("Введите число N: ");
-int N = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine() ?? "0");
 int i = 0;
 
 while(i <= N)
