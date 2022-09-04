e51acb2d4024fdc6a11b09729cd17964044fa620
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 15:14:06 +0700

    Определить количество цифр в числе. Решение с помощью метода

---
+++ EX003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Определить количество цифр в числе
+
+int Resuilt(int num, int i)
+{
+    while(num/10>0)
+    {
+        num = num / 10;
+        i++;
+    }
+    return i;
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine(Resuilt(num, 1));
