64ed1ef997ab5f012d3c580e2998fa38a2ba98ca
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 16:32:47 +0700

    Пользовательский ввод массива

---
+++ EX006/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользовательский ввод массива
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write($"{element} ");
+}
+
+
+Console.WriteLine("Введите длинну массива: ");
+int lenght = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[lenght];
+
+for(int i = 0; i < array.Length;i++)
+{
+    Console.Write($"Enter {i+1} element of array: ");
+    array[i] = int.Parse(Console.ReadLine() ?? "0");
+}
+
+Console.Write("Your array: ");
+PrintArray(array);
