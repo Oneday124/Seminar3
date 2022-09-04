f64b8c6f233c8266d7418a7b5f1f5491b8cbd68f
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 17:58:05 +0700

    Найти кубы чисел от 1 до N. Void метод.

---
+++ EX008_DZ1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти кубы чисел от 1 до N
+
+void Kub(int N)
+{
+    for(int i = 1; i <= N; i++)
+    {
+        int kub = i*i*i;
+        Console.WriteLine(kub);
+    }
+}
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+
+Kub(N);
