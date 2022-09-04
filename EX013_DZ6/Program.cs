356bc3118af30eec634bb1090dbc2115b1e524a8
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 22:39:57 +0700

    Создать массив из 8 элементов, заполненных 0 и 1, вывести их на экран

---
+++ EX013_DZ6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Создать массив из 8 элементов, заполненных 0 и 1, вывести их на экран
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write($"{element} ");
+}
+
+// Console.WriteLine("Введите длинну массива: ");
+// int lenght = int.Parse(Console.ReadLine() ?? "0");
+
+int lenght = 8;
+
+int[] array = new int[lenght];
+for(int i = 0; i < lenght; i++)
+    array[i] = new Random().Next(0, 2);
+
+PrintArray(array);
