afa8c9f6b72ce447e2ce826a63cb9d8d5985594c
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 22:22:57 +0700

    Показать кубы чисел, заканчивающихся на четную цифру

--- EX012_DZ5/Program.cs
+++ EX012_DZ5/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Показать кубы чисел, заканчивающихся на четную цифру
-
+int Cub(int num)
+{
+    int cub = 0;
+    if(num % 10 % 2 == 0)
+    {
+        cub = num * num * num;
+        return cub;
+    }
+    else return -1;
+}
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+int cub = Cub(N);
+if(cub == -1) Console.WriteLine($"Число {N} заканчивается на не четную цифру");
+else 
+    Console.WriteLine($"Куб числа {N} = {cub}");
