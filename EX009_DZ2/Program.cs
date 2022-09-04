c3bc35e27d80522bc64426420193860d3349d743
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 19:14:13 +0700

    Возведите число А в натуральную степень В используя цикл

---
+++ EX009_DZ2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Возведите число А в натуральную степень В используя цикл
+
+int Degree(int A, int B)
+{
+    int sum = 1;
+    int degree = 0;
+    if(B <= 0)
+        return -1;
+    else
+        for(int i = 0; i < B; i++)
+        { 
+            degree = sum * A;
+            sum = degree;
+        }
+    return degree;
+}
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+
+int degree = Degree(A, B);
+Console.WriteLine();
+
+if(B > 0)
+    Console.WriteLine($"Число {A} в степени {B} = {degree}");
+else
+{
+    Console.WriteLine($"Степень {B} не является натуральной");
+}
