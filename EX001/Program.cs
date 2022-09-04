507293024a7df742de21dea4e08e318ce38c18f9
Nikolay <Onedaydejavu124@mail.ru>
2022-09-04 13:52:03 +0700

    Показать таблицу квадратов чисел от 1 до N

--- EX001/Program.cs
+++ EX001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 0;
+
+while(i <= N)
+{
+    int result = i * i;
+    Console.WriteLine($"{i} * {i} = {result}");
+    i++;
+}
+
