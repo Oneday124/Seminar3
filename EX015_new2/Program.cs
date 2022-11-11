316ab6a6deaead29d8e971c0af2140f5bcf814cd
Nikolay <Onedaydejavu124@mail.ru>
2022-11-11 11:23:14 +0700

    Написать программу которая будет выводить простые число от 1 до N. Без списков. Не оконченное решение

---
+++ EX015_new2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу которая будет выводить простые число от 1 до N
+
+Console.Write("ВВедите число N: ");
+int n = int.Parse(Console.ReadLine() ??"0");
+
+for(int current = 1; current <= n; current++)
+{
+    int j = 1;
+    for(int i = 1; i <= current; i++)
+    {
+        if(current % i == 0 && i != 1 && i != current)
+        {
+            Console.Write($"j = {j} ");
+            j++;
+        }
+    }
+    Console.WriteLine();
+}
+
+
+
+
